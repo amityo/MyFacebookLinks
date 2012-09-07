1935208e7e886256f509a2c7330b0279f6d7ff8a
Amit Yogev <amit.yogev90@gmail.com>
2012-09-08 01:38:43 +0300

    first attempt

---
+++ test/Default.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="test._Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+     <link type="text/css" rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <asp:GridView ID="GridView1" runat="server"
+     AllowPaging="True"   
+            AllowSorting="True"
+            AutoGenerateColumns="False"
+            DataKeyNames="LinkID"
+            GridLines="None"
+            CssClass="mGrid"
+            PagerStyle-CssClass="pgr"
+            AlternatingRowStyle-CssClass="alt"
+            Width="50%" HorizontalAlign="Center"
+            >  
+            <Columns>  
+                    <asp:ImageField DataImageUrlField="Picture"  />  
+                    <asp:BoundField DataField="Title" HeaderText="Title" ReadOnly="True"   />  
+                    <asp:HyperLinkField DataNavigateUrlFields="Url" HeaderText="Url"    Text="link"/> 
+                    <asp:BoundField DataField="OwnerComment" HeaderText="Comment" ReadOnly="True"   />  
+                    <asp:BoundField DataField="CreatedTime" HeaderText="CreatedTime" ReadOnly="True"   /> 
+                    <asp:BoundField DataField="Summary" HeaderText="Summary" ReadOnly="True"   />  
+            </Columns>  
+        </asp:GridView>  
+    </div>
+    </form>
+</body>
+</html>
---
+++ test/Default.aspx.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Facebook;
+using System.Net;
+using System.IO;
+
+namespace test
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        string m_clientId = "348858645200865";
+        string m_clientSecret = "b6a16ea47534cf2ef2c2f56b68d8ebea";
+        string m_scope = "read_stream";
+
+        // the getting the token code is from here: http://multitiered.wordpress.com/tag/c/
+
+        private static string GetRedirectUri()
+        {
+            var url = HttpContext.Current.Request.Url;
+            string redirectUri = HttpUtility.UrlEncode(url.GetLeftPart(UriPartial.Authority) + "/");
+            return redirectUri;
+        }
+
+        private string GetAccessToken()
+        {
+            if (HttpRuntime.Cache["access_token"] == null)
+            {
+                Dictionary<string, string> args = GetOauthTokens(Request.Params["code"]);
+                HttpRuntime.Cache.Insert("access_token", args["access_token"], null, DateTime.Now.AddMinutes(Convert.ToDouble(args["expires"])), TimeSpan.Zero);
+            }
+
+            return HttpRuntime.Cache["access_token"].ToString();
+        }
+
+
+        private Dictionary<string, string> GetOauthTokens(string code)
+        {
+            Dictionary<string, string> tokens = new Dictionary<string, string>();
+
+            string redirectUri = GetRedirectUri();
+
+            string url = string.Format("https://graph.facebook.com/oauth/access_token?client_id={0}&redirect_uri={1}&client_secret={2}&code={3}&scope={4}",
+                            m_clientId, redirectUri, m_clientSecret, code, m_scope);
+
+            HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+
+            using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+            {
+
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+
+                string retVal = reader.ReadToEnd();
+
+                foreach (string token in retVal.Split('&'))
+                {
+                    tokens.Add(token.Substring(0, token.IndexOf("=")),
+                    token.Substring(token.IndexOf("=") + 1, token.Length - token.IndexOf("=") - 1));
+                }
+            }
+
+            return tokens;
+
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            GridView1.PageIndexChanging += new GridViewPageEventHandler(GridView1_PageIndexChanging);
+
+            if (Request.Params["code"] == null)
+            {
+                string redirectUri = GetRedirectUri();
+
+                string dialog_url = string.Format("http://www.facebook.com/dialog/oauth?client_id={0}&redirect_uri={1}&scope={2}",
+                                m_clientId, redirectUri, m_scope);
+                Response.Redirect(dialog_url);
+            }
+            else
+            {
+                FqlToLinqSample();
+            }
+        }
+
+        private void FqlToLinqSample()
+        {
+            var fb = new FacebookClient(GetAccessToken());
+            var db = new FacebookDataContext(fb);
+            
+            var links = (from link in db.Link
+                        where link.Owner == (long?)db.Me
+                        select link).ToList();
+
+            var youtube = from link in links
+                         where link.Url.Contains("youtube")
+                         orderby link.CreatedTime descending
+                         select link;
+
+            var list = youtube.ToList();
+            list.ForEach(x =>
+            {
+                if (x.Url.Contains("gdata"))
+                {
+                    x.Url = "http://www.youtube.com/watch?v=" + x.Url.Split('/').Last();
+                }
+            });
+
+            var fixedList = (from link in youtube
+                             select new { link.LinkId, link.OwnerComment, link.CreatedTime, link.Title, link.Summary, link.Url, link.Picture }).ToList();
+            GridView1.DataSource = fixedList;
+            GridView1.DataBind();
+        }
+
+        void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            GridView1.PageIndex = e.NewPageIndex;
+            GridView1.DataBind();
+        }
+
+    }
+}
