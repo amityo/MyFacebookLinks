c14433b35b865b74ee026b0662f33f7e188fbad6
Amit Yogev <amit.yogev90@gmail.com>
2012-09-19 01:37:26 +0300

    encapsulating facebook authentication

--- test/Default.aspx.cs
+++ test/Default.aspx.cs
@@ -15,89 +15,22 @@ namespace test
 {
     public partial class _Default : System.Web.UI.Page
     {
-        string m_clientId;
-        string m_clientSecret;
-        string m_redirectUri ="";
-        string m_scope = "read_stream";
         FacebookDataContext db;
-        // the getting the token code is from here: http://multitiered.wordpress.com/tag/c/
-
-        private string GetRedirectUri()
-        {
-            return HttpUtility.UrlEncode(m_redirectUri);
-
-
-            var url = HttpContext.Current.Request.Url;
-            string urll = url.GetLeftPart(UriPartial.Path).Split(':')[0] + url.GetLeftPart(UriPartial.Path).Split(':')[1];
-            string redirectUri = HttpUtility.UrlEncode(urll + "/");
-            return redirectUri;
-        }
-
-        private string GetAccessToken()
-        {
-            if (Session["access_token"] == null)
-            {
-                Dictionary<string, string> args = GetOauthTokens(Request.Params["code"]);
-                Session.Add("access_token", args["access_token"]);
-            }
-
-            return Session["access_token"].ToString();
-        }
-
-
-        private Dictionary<string, string> GetOauthTokens(string code)
-        {
-            Dictionary<string, string> tokens = new Dictionary<string, string>();
-
-            string redirectUri = GetRedirectUri();
-
-            string url = string.Format("https://graph.facebook.com/oauth/access_token?client_id={0}&redirect_uri={1}&client_secret={2}&code={3}&scope={4}",
-                            m_clientId, redirectUri, m_clientSecret, code, m_scope);
-
-            HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
-
-            using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
-            {
-
-                StreamReader reader = new StreamReader(response.GetResponseStream());
-
-                string retVal = reader.ReadToEnd();
-
-                foreach (string token in retVal.Split('&'))
-                {
-                    tokens.Add(token.Substring(0, token.IndexOf("=")),
-                    token.Substring(token.IndexOf("=") + 1, token.Length - token.IndexOf("=") - 1));
-                }
-            }
-
-            return tokens;
-
-        }
 
         protected void Page_Load(object sender, EventArgs e)
         {
             string prefix = ConfigurationManager.AppSettings["mode"];
-            m_redirectUri = ConfigurationManager.AppSettings[prefix + "_redirectUri"];
-            m_clientId= ConfigurationManager.AppSettings[prefix + "_clientId"];
-            m_clientSecret = ConfigurationManager.AppSettings[prefix + "_clientSecret"];
-
-
+            string mRedirectUri = ConfigurationManager.AppSettings[prefix + "_redirectUri"];
+            string clientId = ConfigurationManager.AppSettings[prefix + "_clientId"];
+            string clientSecret = ConfigurationManager.AppSettings[prefix + "_clientSecret"];
             GridView1.PageIndexChanging += GridView1_PageIndexChanging;
-                
-            if (Request.Params["code"] == null)
-            {
-                string redirectUri = GetRedirectUri();
 
-                string dialog_url = string.Format("http://www.facebook.com/dialog/oauth?client_id={0}&redirect_uri={1}&scope={2}",
-                                m_clientId, redirectUri, m_scope);
-                Response.Redirect(dialog_url);
-            }
-            else
-            {
-                var fb = new FacebookClient(GetAccessToken());
-                db = new FacebookDataContext(fb);
-                FqlToLinqSample();
-            }
+            FacebookAuthentication auth = new FacebookAuthentication(clientId, clientSecret, mRedirectUri);
+            string accessToken = auth.Login(Request.Params["code"]);
+
+            var fb = new FacebookClient(accessToken);
+            db = new FacebookDataContext(fb);
+            FqlToLinqSample();
         }
 
         public void RowDataBounded(object sender, GridViewRowEventArgs eventArgs)
---
+++ test/FacebookAuthentication.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Net;
+
+namespace test
+{
+    public class FacebookAuthentication
+    {
+        #region Data Members
+
+        private string mClientId;
+        private string mClientSecret;
+        private string mRedirectUri;
+        private string mScope = "read_stream";
+        private HttpContext mPage;
+        #endregion
+
+        #region Initialize
+
+        public FacebookAuthentication(string clientId, string clientSecret,string redirectUri)
+        {
+            mClientId = clientId;
+            mClientSecret = clientSecret;
+            mRedirectUri = GetRedirectUri(redirectUri);
+            mPage = HttpContext.Current;
+        }
+
+        private string GetRedirectUri(string redirectUri)
+        {
+            return HttpUtility.UrlEncode(redirectUri);
+        }
+        #endregion
+
+        /// <summary>
+        /// login to facebook user and returns token to access the user's facebook information
+        /// </summary>
+        /// <param name="code"></param>
+        /// <returns>the access token</returns>
+        public string Login(string code)
+        {
+            if (string.IsNullOrEmpty(code))
+            {
+                RedirectEmptyCode();
+            }
+
+            if (mPage.Session["access_token"] == null)
+            {
+                string access_token = GetAccessToken(code);
+                mPage.Session.Add("access_token", access_token);
+            }
+
+            return mPage.Session["access_token"].ToString();
+        }
+
+        #region Private Methods
+
+        private void RedirectEmptyCode()
+        {
+            string dialog_url = string.Format("http://www.facebook.com/dialog/oauth?client_id={0}&redirect_uri={1}&scope={2}",
+                            mClientId, mRedirectUri, mScope);
+            mPage.Response.Redirect(dialog_url);
+        }
+        private string GetAccessToken(string code)
+        {
+            string url = string.Format("https://graph.facebook.com/oauth/access_token?client_id={0}&redirect_uri={1}&client_secret={2}&code={3}&scope={4}",
+                            mClientId, mRedirectUri, mClientSecret, code, mScope);
+
+            HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+            using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+            using(StreamReader reader = new StreamReader(response.GetResponseStream()))
+            {
+                string responseString = reader.ReadToEnd();
+
+                string key = "access_token=";
+                int index = responseString.IndexOf(key) + key.Length;
+                int count = responseString.IndexOf("&", index) - index;
+
+                return responseString.Substring(index, count);
+            }
+        }
+        #endregion
+
+
+    }
+}
